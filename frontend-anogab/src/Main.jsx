f75812bfa85164627bd28953f7d9d62955611cc3
vivek9545 <99834865+vivek9545@users.noreply.github.com>
2025-09-06 21:56:21 +0530

    Update and rename main.jsx to Main.jsx

--- frontend-anogab/src/Main.jsx
+++ frontend-anogab/src/Main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import React from 'react'
-import './index.css'
+import './Index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
